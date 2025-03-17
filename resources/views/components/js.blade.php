5648f7dabb0884abeff326d464087d0923bacd5c
Sashkoid <sashkenzo@gmail.com>
2025-03-17 15:07:30 +0200

    second commit. loginpage updated

--- resources/views/components/js.blade.php
+++ resources/views/components/js.blade.php
@@ -1,3 +1,3 @@
 <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"> </script>
 <script src="{{asset('bootstrap/js/popper.min.js')}}"> </script>
-<script src="{{asset('assets/js/dashboard.js')}}"> </script>
+<script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"> </script>
