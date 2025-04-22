cc33cd8b66d05aa0788c1d1a4c45897aafa90058
Sashkoid <sashkenzo@gmail.com>
2025-04-22 22:37:04 +0300

    minor bugs

---
+++ resources/views/errors/402.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+        <div class="error-container">
+            <h1 class="display-1">Oops!</h1>
+            <h2 class="display-4">402 Error</h2>
+            <div class="error-details mb-4">
+                Sorry, an error has occurred. The requested page was not found!
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,14 @@
+@extends('errors::minimal')
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+        <div class="error-container">
+            <h1 class="display-1">Oops!</h1>
+            <h2 class="display-4">403 Error</h2>
+            <div class="error-details mb-4">
+                Sorry, an error has occurred. The requested page was not found!
+            </div>
+        </div>
+    </div>
+@endsection
